85173d4bb37b6c66c1835e3d3c9e4219c7e46281
gioadami <giordano.adami@gmail.com>
2015-06-09 13:46:44 +0200

    Fixed wsdlLocation.

--- src/main/java/it/infotn/epu/DomandaEPUService.java
+++ src/main/java/it/infotn/epu/DomandaEPUService.java
@@ -17,7 +17,7 @@ import javax.xml.ws.WebServiceFeature;
  * Generated source version: 2.1
  * 
  */
-@WebServiceClient(name = "DomandaEPUService", targetNamespace = "http://epu.infotn.it", wsdlLocation = "https://epu-test.infotn.it/epu/DomandaEPUService?wsdl")
+@WebServiceClient(name = "DomandaEPUService", targetNamespace = "http://epu.infotn.it", wsdlLocation = "https://ws-p.servizi.infotn.it/epu/DomandaEPUService?wsdl")
 public class DomandaEPUService
     extends Service
 {
